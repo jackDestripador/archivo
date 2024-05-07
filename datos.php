3e1206de64c9046dc9a4c434eb59a7f579db41bb
jesus cisneros <jesus.cisneros@stps.gob.mx>
2024-05-07 17:33:05 -0600

    modificacion al diseño

--- datos.php
+++ datos.php
@@ -1,10 +1,5 @@
 <?php
-    //nombre del archivo
-    //$ruta="archivos/".$_POST['unidadadministartiva']."-RFV-1-2024.pdf";
-    //$ruta="archivos/".$_FILES['archivo']['name'];
-    //$ruta="archivos/".$_POST['unidadadministartiva']."-".$_POST['numeroexpedinete']."-".$_POST['anoexpediente'].".pdf";
     $ruta="archivos/".$_POST['unidadadministartiva']."-".$_POST['areaadministrativa']."-".$_POST['numeroexpedinete']."-".$_POST['anoexpediente'].".pdf";
-    //$ruta="archivos/".$_POST['anoexpediente']."-RFV-1-2024.pdf";
     if(move_uploaded_file($_FILES['archivo']['tmp_name'],$ruta)){
         echo "Archivo subido con exito";
     }else{
