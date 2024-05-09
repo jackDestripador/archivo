a3a5a9830ff144625ed443a16f947f740b954361
jesus cisneros <jesus.cisneros@stps.gob.mx>
2024-05-09 13:19:03 -0600

    se agrego nuevo campos

--- datos.php
+++ datos.php
@@ -1,5 +1,5 @@
 <?php
-    $ruta="archivos/".$_POST['unidadadministartiva']."-".$_POST['areaadministrativa']."-".$_POST['numeroexpedinete']."-".$_POST['anoexpediente'].".pdf";
+    $ruta="archivos/".$_POST['unidadadministartiva']."-".$_POST['areaadministrativa']."-".$_POST['numeroexpedinete']."-".$_POST['ae']."-".$_POST['ac'].".pdf";
     if(move_uploaded_file($_FILES['archivo']['tmp_name'],$ruta)){
         echo "Archivo subido con exito";
     }else{
